2478f8709ae618f3b212c2111750d219304e250c
benniwilson <benniwilson7@gmail.com>
2024-11-19 16:38:37 +0000

    refactor: refactored the Input class to make different methods to validate each input in terms of plateau size, position, and instruction. Generating new tests for the new methods in the process

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -4,6 +4,7 @@ import InputParsing.DirectionParser;
 import InputParsing.InstructionParser;
 import InputParsing.PlateauSizeParser;
 import InputParsing.PositionParser;
+import Logic.Direction;
 import Logic.Instruction;
 
 import java.util.ArrayList;
@@ -19,11 +20,10 @@ public class Input {
     List<String> positionParserStrings = new ArrayList<>();
     InstructionParser instructionParser;
     List<String> instructionParserStrings = new ArrayList<>();
-    int numberOfRovers;
+    List<String> input;
 
-
-    public int getNumberOfRovers() {
-        return numberOfRovers;
+    public Input(List<String> input) {
+        this.input = input;
     }
 
     public List<String> getPositionParserStrings() {
@@ -46,33 +46,79 @@ public class Input {
         return instructionParser;
     }
 
-    public boolean inputValidator(List<String> input){
-        //inputValidator validates that the input is suitable for use, using its own logic in terms of
-        //the input size and the parser classes logic for their own individual inputs.
+    public boolean inputValidator() {
         if (input.size() < 3 || input.size() % 2 == 0){
             return false;
         }
-        plateauSizeParser = new PlateauSizeParser(input.get(0));
-        if (deepEquals(plateauSizeParser.parsePlateauSize(), new int[0][0])){
-            System.out.println("The plateau has no size, try again...");
+        plateauSizeInputValidator();
+        positionParserInputValidator();
+        instructionParserInputValidator();
+        return plateauSizeInputValidator() || positionParserInputValidator() || instructionParserInputValidator();
+    }
+
+    public boolean plateauSizeInputValidator(){
+        plateauSizeParser = new PlateauSizeParser(input.getFirst());
+        try {
+            int[][] plateauSize = plateauSizeParser.parsePlateauSize();
+            if (deepEquals(plateauSize, new int[0][0])){
+                throw new IllegalArgumentException("The plateau size is invalid, please try another input...");
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
             return false;
         }
-        instructionParser =  new InstructionParser();
+        return true;
+    }
+
+    public boolean positionParserInputValidator(){
+        int y;
+        int x;
+        Direction direction;
+        plateauSizeParser = new PlateauSizeParser(input.getFirst());
+        int xMax = plateauSizeParser.parsePlateauSize()[0].length-1;
+        int yMax = plateauSizeParser.parsePlateauSize()[1].length-1;
         positionParser = new PositionParser();
-        numberOfRovers = (input.size()-1)/2;
-        for (int i = 1; i < input.size(); i++) {
-            if (i % 2 == 0 && instructionParser.parseInstruction(input.get(i)) == null) {
-                System.out.println("The instructions for the rover are incomplete, try again...");
-                return false;
-            } else if (i % 2 == 0) {
-                instructionParserStrings.add(input.get(i));
-            } else if (positionParser.parseDirection(input.get(i)) == null ||
-                    positionParser.parseXCoordinate(input.get(i)) == -1 ||
-                    positionParser.parseYCoordinate(input.get(i)) == -1) {
-                System.out.println("The starting position for the rover is incorrect, try again...");
-                return false;
-            } else {
+        for (int i = 1; i < input.size(); i++){
+            if (i % 2 != 0){
+                try {
+                y = positionParser.parseYCoordinate(input.get(i));
+                x = positionParser.parseXCoordinate(input.get(i));
+                direction = positionParser.parseDirection(input.get(i));
+                if (y == -1 || x == -1 || direction == null){
+                    throw new IllegalArgumentException("The rover starting position is invalid, please " +
+                            "try another input...");
+                }else if (x > xMax){
+                    throw new IllegalArgumentException("The starting x coordinate is greater than the size of the" +
+                            " plateau, please try another input...");
+                }else if(y > yMax){
+                    throw new IllegalArgumentException("The starting y coordinate is greater than the size of the" +
+                            " plateau, please try another input...");
+                }
                 positionParserStrings.add(input.get(i));
+                }catch(IllegalArgumentException e){
+                    System.out.println(e.getMessage());
+                    return false;
+            }
+        }
+        }
+        return true;
+    }
+
+    public boolean instructionParserInputValidator(){
+        instructionParser = new InstructionParser();
+        for (int i = 1; i < input.size(); i++){
+            if (i % 2 == 0){
+                try{
+                    List<Instruction> instructions = instructionParser.parseInstruction(input.get(i));
+                    if (instructions == null){
+                        throw new IllegalArgumentException("The instructions passed to the rover are invalid" +
+                                ", please try again...");
+                    }
+                    instructionParserStrings.add(input.get(i));
+                }catch(IllegalArgumentException e){
+                    System.out.println(e.getMessage());
+                    return false;
+                }
             }
         }
         return true;
