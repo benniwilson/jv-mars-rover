d9004ba7beaf22adb89ee534b203f13ac96156b7
benniwilson <benniwilson7@gmail.com>
2024-11-19 15:09:28 +0000

    refactor: Changed the code in the isMovementAllowed method so that the x and y values are correct such that the method performs as desired

--- src/main/java/Logic/PlateauSize.java
+++ src/main/java/Logic/PlateauSize.java
@@ -1,4 +1,4 @@
-package Main;
+package Logic;
 
 public class PlateauSize {
     int[][] plateauSize;
@@ -11,8 +11,8 @@ public class PlateauSize {
         if (position == null || instruction == null){
             return false;
         }
-        int x = plateauSize[0].length;
-        int y = plateauSize[1].length;
+        int x = plateauSize[0].length-1;
+        int y = plateauSize[1].length-1;
         if (position.getFacing() == Direction.E && position.getX() == x){
             return false;
         }else if (position.getFacing() == Direction.W && position.getX() == 0){
