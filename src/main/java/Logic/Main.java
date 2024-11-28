9e1a6a451126041fae9c0d7286dfdc9e2343dd83
benniwilson <benniwilson7@gmail.com>
2024-11-28 18:58:32 +0000

    refactor: Changed the code in the main class so that when it is ran the user is prompted for inputs in the terminal using the InputGetter methods

--- src/main/java/Logic/Main.java
+++ src/main/java/Logic/Main.java
@@ -1,11 +1,13 @@
 package Logic;
 
+import Input.InputGetter;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> listInput = List.of("5 5","2 2 N","LMLMLMLMM","4 3 E","MMRMMRMRRM", "2 1 S", "MRMRMLMLM");
         Controller controller = new Controller();
-        controller.movement(listInput);
+        controller.roverMovement();
     }
 }
